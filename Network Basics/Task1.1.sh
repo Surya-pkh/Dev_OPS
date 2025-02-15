3bae52df7a283fe05944b01671a809a16a41bc5c
Surya prakash C <suryaprakash27032001@gmail.com>
2025-02-15 22:54:33 +0530

    Create Task1.1.sh

---
+++ Network Basics/Task1.1.sh
@@ -0,0 +1,14 @@
+#Get me the IP address of a particular domain (guvi.in). How do I find my CPU/memory usage of my server?. Test the connectivity between 2 nodes?
+
+# finding the IP address
+nslookup guvi.in
+
+#find Cpu/memory usage
+top
+
+# Connectivity between two nodes
+ping 191.168.31.1
+
+# ping Host IP(xxxx.xxxx.xxxx.xxxx)
+
+
